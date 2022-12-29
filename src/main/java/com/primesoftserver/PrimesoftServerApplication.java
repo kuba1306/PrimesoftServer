bc39dc5c81eb88e12ea5af88402301243e7c2b79
kuba1306 <jakubpanienski@wp.pl>
2022-12-29 23:50:50 +0100

    first commit

---
+++ src/main/java/com/primesoftserver/PrimesoftServerApplication.java
@@ -0,0 +1,13 @@
+package com.primesoftserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimesoftServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimesoftServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/primesoftserver/DBManagerTestSuite.java
@@ -0,0 +1,2 @@
+package com.primesoftserver;public class DBManagerTestSuite {
+}
